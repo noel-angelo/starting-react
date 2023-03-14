6d7894a80e974f340fa27a9589630ed7ee9ecc8f
Noel Angelo Magalang <noel.magalang@awsys-i.com>
2023-03-14 08:56:08 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
+import React from "react";
+import PropTypes from "prop-types";
 import './App.css';
 
+const PokemonRow = ({ pokemon, onSelect }) => (
+  <tr>
+    <td>{ pokemon.name.english }</td>
+    <td>{ pokemon.type.join(", ") }</td>
+    <td>
+      <button
+        onClick={()=> onSelect(pokemon)}>Select!</button>
+    </td>
+  </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
+
+const PokemonInfo = ({ name, base}) => (
+  <div>
+    <h1>{ name.english }</h1>
+    <table>
+      <tbody>
+        {
+          Object.keys(base).map((key) => (
+            <tr key={key}>
+              <td>{ key }</td>
+              <td>{ base[key] }</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+  </div>
+)
+
+PokemonInfo.propTypes = {
+  name: PropTypes.shape({
+    english: PropTypes.string,
+  }),
+  base: PropTypes.shape({
+    HP: PropTypes.number.isRequired,
+    Attack: PropTypes.number.isRequired,
+    Defense: PropTypes.number.isRequired,
+    "Sp. Attack": PropTypes.number.isRequired,
+    "Sp. Defense": PropTypes.number.isRequired,
+    Speed: PropTypes.number.isRequired,
+  }),
+}
+
 function App() {
+  const [filter, filterSet] = React.useState("");
+  const [pokemon, pokemonSet] = React.useState([]);
+  const [selectedItem, selectedItemSet] = React.useState(null);
+  
+  React.useEffect(() => {
+    fetch("http://localhost:3000/starting-react/pokemon.json")
+      .then(resp => resp.json())
+      .then(data => pokemonSet(data));
+  }, [filter]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem"
+    }}>
+      <h1 className='title'>Pokemon Search</h1>
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "70% 30%",
+        gridColumnGap: "1rem"
+      }}>
+        <div>
+          <input
+            value={filter}
+            onChange={ evt => filterSet(evt.target.value) }
+          />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              { pokemon
+                .filter((pokemon) => pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+                .slice(0, 20)
+                .map(pokemon => (
+                <PokemonRow
+                  pokemon={ pokemon }
+                  key={ pokemon.id }
+                  onSelect={ (pokemon) => selectedItemSet(pokemon) }
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+        { selectedItem && <PokemonInfo { ...selectedItem } /> }
+      </div>
     </div>
   );
 }
